967995135297aedebb67eba8790c87abba17257f
vozoid <62110949+vozoid@users.noreply.github.com>
2022-01-28 14:14:10 +0500

    Create framework.lua

---
+++ framework.lua
@@ -0,0 +1,92 @@
+local inputService = game:GetService("UserInputService")
+local tweenService = game:GetService("TweenService")
+local runService = game:GetService("RunService")
+local coreGui = game:GetService("CoreGui")
+
+local framework = {signal = loadstring(game:HttpGet("https://raw.githubusercontent.com/vozoid/Signal-library/main/main.lua"))()}
+
+function framework.format_table(tbl)
+    if tbl then
+        local oldtbl = tbl
+        local newtbl = {}
+        local formattedtbl = {}
+
+        for option, v in next, oldtbl do
+            newtbl[option:lower()] = v
+        end
+
+        setmetatable(formattedtbl, {
+            __newindex = function(t, k, v)
+                rawset(newtbl, k:lower(), v)
+            end,
+            __index = function(t, k, v)
+                return newtbl[k:lower()]
+            end
+        })
+
+        return formattedtbl
+    else
+        return {}
+    end
+end
+
+framework = framework.format_table(framework)
+
+framework.forcedProperties = {}
+
+function framework.import(branch, file)
+    return loadstring(game:HttpGetAsync(("https://raw.githubusercontent.com/vozoid/ui-framework/%s/%s.lua"):format(branch, file .. ".lua")))()
+end
+
+function framework:Create(object)
+    local object = Instance.new(class)
+
+    for prop, v in next, properties do
+        object[prop] = v
+    end
+
+    for prop, v in next, framework.forcedProperties do
+        pcall(function()
+            object[prop] = v
+        end)
+    end
+    
+    return obj
+end
+
+function framework:Tween(obj, info, properties, callback)
+    local anim = tweenService:Create(obj, TweenInfo.new(unpack(info)), properties)
+    anim:Play()
+
+    if callback then
+        anim.Completed:Connect(callback)
+    end
+end
+
+function framework:Dragify(object, speed)
+    local start, objectPosition, dragging
+
+    speed = speed or 0
+
+    object.InputBegan:Connect(function(input)
+        if input.UserInputType == Enum.UserInputType.MouseButton1 then
+            dragging = true
+            start = input.Position
+            objectPosition = object.Position
+        end
+    end)
+
+    object.InputEnded:Connect(function(input)
+        if input.UserInputType == Enum.UserInputType.MouseButton1 then 
+            dragging = false
+        end
+    end)
+
+    inputService.InputChanged:Connect(function(input)
+        if input.UserInputType == Enum.UserInputType.MouseMovement and dragging then   
+            framework:Tween(object, {speed}, {Position = UDim2.new(objectPosition.X.Scale, objectPosition.X.Offset + (input.Position - start).X, objectPosition.Y.Scale, objectPosition.Y.Offset + (input.Position - start).Y)})
+        end
+    end)
+end
+
+return framework
