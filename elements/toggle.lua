4ec80b2be6b16bc259ee8cb614990abd966b014f
vozoid <62110949+vozoid@users.noreply.github.com>
2022-01-28 14:29:40 +0500

    Create toggle.lua

---
+++ elements/toggle.lua
@@ -0,0 +1,40 @@
+local framework = loadstring(game:HttpGet("https://raw.githubusercontent.com/vozoid/ui-framework/main/framework.lua"))()
+
+function Toggle(object)
+    local toggled = false
+
+    local toggleTypes = {
+        Toggled = framework.signal.new(), 
+        Enabled = framework.signal.new(), 
+        Disabled = framework.signal.new()
+    }
+
+    toggleTypes = framework.format_table(toggleTypes)
+
+    local function toggle()
+        toggled = not toggled
+        toggleTypes.Toggled:Fire(toggled)
+
+        if toggled then
+            toggleTypes.Enabled:Fire(toggled)
+        else
+            toggleTypes.Disabled:Fire(toggled)
+        end
+    end
+
+    object.InputBegan:Connect(function(input)
+        if input.UserInputType == Enum.UserInputType.MouseButton1 then
+            toggle()
+        end
+    end)
+
+    function toggleTypes:Toggle(bool)
+        if toggled ~= bool then
+            toggle()
+        end
+    end
+
+    return toggleTypes
+end
+
+return Toggle
