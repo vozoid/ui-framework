8b55aab06d58aa1512603cf7326a01307e099dd3
vozoid <62110949+vozoid@users.noreply.github.com>
2022-01-28 14:32:57 +0500

    Update module.lua

--- module.lua
+++ module.lua
@@ -1,4 +1,7 @@
 local framework = loadstring(game:HttpGet("https://raw.githubusercontent.com/vozoid/ui-framework/main/framework.lua"))()
-local framework.button = framework.import("button")
+framework.button = framework.import("button")
+framework.toggle = framework.import("toggle")
+framework.box = framework.import("box")
+
 
 return framework
