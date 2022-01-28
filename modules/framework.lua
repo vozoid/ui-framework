03579f6c0d10785e02654a7e91456e55dd42ecf0
vozoid <62110949+vozoid@users.noreply.github.com>
2022-01-28 14:55:06 +0500

    Update framework.lua

--- modules/framework.lua
+++ modules/framework.lua
@@ -3,7 +3,7 @@ local tweenService = game:GetService("TweenService")
 local runService = game:GetService("RunService")
 local coreGui = game:GetService("CoreGui")
 
-local framework = {signal = loadstring(game:HttpGet("https://raw.githubusercontent.com/vozoid/Signal-library/main/main.lua"))()}
+local framework = {signal = loadstring(readfile("ui-framework/modules/signal.lua"))()}
 
 function framework.format_table(tbl)
     if tbl then
