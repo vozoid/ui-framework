a1eb54b4be775e57c98255b4a6d7e7e379e5f7fd
vozoid <62110949+vozoid@users.noreply.github.com>
2022-01-28 14:31:36 +0500

    Create box.lua

---
+++ elements/box.lua
@@ -0,0 +1,24 @@
+local framework = loadstring(game:HttpGet("https://raw.githubusercontent.com/vozoid/ui-framework/main/framework.lua"))()
+
+function Box(object)
+    local boxTypes = {
+        Updated = framework.signal.new()
+    }
+
+    boxTypes = framework.format_table(boxTypes)
+
+    local function updateBox()
+        boxTypes.Updated:Fire(object.Text)
+    end
+
+    object.FocusLost:Connect(updateBox)
+
+    function boxTypes:Set(text)
+        object.Text = text
+        updateBox()
+    end
+
+    return boxTypes
+end
+
+return Box
