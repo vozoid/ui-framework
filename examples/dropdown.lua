89b99b8efbc695f20e929d52ba74b56365dd3301
vozoid <62110949+vozoid@users.noreply.github.com>
2022-01-29 12:52:08 +0500

    Create dropdown.lua

---
+++ examples/dropdown.lua
@@ -0,0 +1,49 @@
+local gui = Instance.new("ScreenGui", game.CoreGui)
+
+local frame = Instance.new("Frame", gui)
+frame.Size = UDim2.new(0, 200, 0, 200)
+frame.Position = UDim2.new(0, 50, 0, 50)
+
+local list = Instance.new("UIListLayout", frame)
+
+local template = Instance.new("TextButton")
+template.Size = UDim2.new(1, 0, 0, 20)
+
+local dropdown = framework.dropdown(frame, {"Option 1", "Option 2"}, template)
+
+dropdown.Updated:connect(function(option, obj)
+    print(option, object)
+end)
+
+dropdown.NoneSelected:connect(function()
+    print("NONE")
+end)
+
+dropdown.Selected:connect(function(option, obj)
+    obj.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+end)
+
+dropdown.Deselected:connect(function(option, obj)
+    obj.BackgroundColor3 = Color3.fromRGB(100, 100, 100)
+end)
+
+dropdown.Removed:connect(function(opt, obj)
+    print(opt)
+end)
+
+drop:Remove("Option 1")
+
+dropdown.Added:connect(function(option)
+    print(option)
+end)
+
+drop:Add("Option 3")
+drop:Set("Option 2")
+
+dropdown.Refreshed:connect(function(tbl)
+    table.foreach(tbl, print)
+end)
+
+drop:Refresh({"Option 4",'Option 5'})
+
+drop:Set()
