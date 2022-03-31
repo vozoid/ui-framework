5930c68ed9edd562d5dbe1e7f5889edcaa468c59
vozoid <62110949+vozoid@users.noreply.github.com>
2022-03-31 15:08:26 +0500

    Create signal.lua

---
+++ signal.lua
@@ -0,0 +1,69 @@
+-- quenty made this
+local HttpService = game:GetService("HttpService")
+local ENABLE_TRACEBACK = false
+--
+local Signal = {}
+Signal.__index = Signal
+Signal.ClassName = "Signal"
+--
+function Signal.new()
+	local self = setmetatable({}, Signal)
+	self._bindableEvent = Instance.new("BindableEvent")
+	self._argMap = {}
+	self._source = ENABLE_TRACEBACK and debug.traceback() or ""
+	self._bindableEvent.Event:Connect(function(key)
+		self._argMap[key] = nil
+		if (not self._bindableEvent) and (not next(self._argMap)) then
+			self._argMap = nil
+		end
+	end)
+	return self
+end
+--
+function Signal:Fire(...)
+	if not self._bindableEvent then
+		return
+	end
+	local args = table.pack(...)
+	local key = HttpService:GenerateGUID(false)
+	self._argMap[key] = args
+	self._bindableEvent:Fire(key)
+end
+--
+function Signal:Connect(handler)
+	return self._bindableEvent.Event:Connect(function(key)
+		local args = self._argMap[key]
+		if args then
+			handler(table.unpack(args, 1, args.n))
+		end
+	end)
+end
+
+function Signal:connect(handler)
+	return self._bindableEvent.Event:Connect(function(key)
+		local args = self._argMap[key]
+		if args then
+			handler(table.unpack(args, 1, args.n))
+		end
+	end)
+end
+--
+function Signal:Wait()
+	local key = self._bindableEvent.Event:Wait()
+	local args = self._argMap[key]
+	if args then
+		return table.unpack(args, 1, args.n)
+	else
+		return nil
+	end
+end
+--
+function Signal:Destroy()
+	if self._bindableEvent then
+		self._bindableEvent:Destroy()
+		self._bindableEvent = nil
+	end
+	setmetatable(self, nil)
+end
+--
+return Signal
